9e576f6a90ed0c8c7522990e374476cba2ffeda5
canicemichael <canicemichael@gmail.com>
2022-06-25 12:23:49 +0100

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from blog import views
-from blog.views import MyView, PostListView
+from blog.views import PostListView, PostCreateView, PostDeleteView, PostUpdateView, PostDetailView
+
+app_name = "blog"
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path('mine/', MyView.as_view(), name="my-view"),
-    path("post/", PostListView.as_view(), name="article-list"),
-    path("blog/<name>", views.hello_there, name="hello_there"),
+    path("", PostListView.as_view(), name="all"),
+    path("create/", PostCreateView.as_view(), name="post_create"),
+    path("delete/<slug:slug>", PostDeleteView.as_view(), name="post_delete"),
+    path("update/<slug:slug>", PostUpdateView.as_view(), name="post_update"),
+    path("read/<slug:slug>", PostDetailView.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,63 +1,32 @@
-from nntplib import ArticleInfo
-from django.shortcuts import render
-from django.http import HttpResponse
-import re
-from django.utils.timezone import datetime
-
-
+from django.urls import reverse_lazy
 from django.views import View
-from django.utils import timezone
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from blog.models import Post
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
-    paginate_by = 100 # if pagination is desired
+    template_name = "post_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        return context
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = "post_form.html"
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
 
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def home(request):
-    return HttpResponse("Hello, Django!")
-
-class MyView(View):
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, World')
-
-def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions, URL arguments can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = "post_confirm_delete.html"
